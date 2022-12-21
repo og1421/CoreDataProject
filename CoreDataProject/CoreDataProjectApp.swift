0a82cd2e18b59b41873448cec8acd52844b4a704
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2022-12-21 18:49:03 -0300

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Orlando Moraes Martins on 21/12/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
